4eca1069f3193a0192fea21deb29f34fcd2a696f
Alain Igban <apigban@gmail.com>
2018-09-02 04:47:40 -0400

    created module pageQueue.py containing decorator redisWrite

---
+++ pageQueue.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python3.7
+
+import redis
+import functools
+import login_redis as lr
+from datetime import datetime
+
+
+redis_dump = redis.StrictRedis(
+    host = lr.login['ip'],
+    port = lr.login['port'],
+    password = lr.login['password'],
+    db = lr.login['db']
+    )
+
+def redisWrite(function_toWrite):
+    """
+    Writes to redis
+    """
+
+    @functools.wraps(function_toWrite)
+    def wrapper_redisWrite(*args, **kwargs):
+        value = function_toWrite(*args, **kwargs)       # run function wrapped
+        value[2] = int(1000*value[2].timestamp())           #transform value[2] to timestamp to milliseconds and to type int
+        print(value)
+        return value
+
+    #redis_dump.rpush('',value)
+    return wrapper_redisWrite
