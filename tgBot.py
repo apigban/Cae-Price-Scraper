370975064ec94d8067a22beacb0db897f23f8105
Alain Igban <apigban@gmail.com>
2018-09-02 09:05:10 -0400

    last commit for 20180902

--- tgBot.py
+++ tgBot.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python3.7
 
 import requests
+import re
 from datetime import datetime
 from tg_token import token
 
@@ -37,13 +38,18 @@ class BotHandler:
         if len(get_result) > 0:
             last_update = get_result[-1]
         else:
-            last_update = get_result#[len(get_result)]
+            last_update = get_result[len(get_result)]
         return last_update
 
 
-greet_bot = BotHandler(token)
+
+price_bot = BotHandler(token)
 
 greetings = ('hello', 'hi', 'greetings', 'wassup')
+
+commands = ('getPrice', 'getItem', 'getLink')
+
+item = None
 
 now = datetime.now()
 
@@ -56,25 +62,21 @@ def main():
 
 
     while True:
-        greet_bot.getUpdates(new_offset)
-        last_update = greet_bot.getLastUpdate()
+        price_bot.getUpdates(new_offset)
+        last_update = price_bot.getLastUpdate()
 
         last_update_id = last_update['update_id']
         last_chat_text = last_update['message']['text']
+        print(last_chat_text)
         last_chat_id = last_update['message']['chat']['id']
         last_chat_name = last_update['message']['chat']['first_name']
 
-        if last_chat_text.lower() in greetings and today == now.day and 6<= hour < 12:
-            greet_bot.reply(last_chat_id, f'Good Morning {last_chat_name}')
-            today +=1
-        elif last_chat_text.lower() in greetings and today == now.day and 12 <= hour < 17:
-            greet_bot.reply(last_chat_id, f'Good Afternoon {last_chat_name}')
-            today += 1
-        elif last_chat_text.lower() in greetings and today == now.day and 17 <= hour < 23:
-            greet_bot.reply(last_chat_id, f'Good Evening  {last_chat_name}')
-            today += 1
+        if last_chat_text.lower() == 'getprice':
+            price_bot.reply(last_chat_id, f'Looking for price of {item}')
+
 
         new_offset = last_update_id + 1
+        print(last_update)
 
 if __name__ == '__main__':
     try:
