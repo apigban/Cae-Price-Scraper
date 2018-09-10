df23b6884c5af9eba597053952c04ca403b75179
Alain Igban <apigban@gmail.com>
2018-09-10 09:28:44 -0400

    added input error detection, nonalpha and empty

---
+++ inputValidator.py
@@ -0,0 +1,47 @@
+#!/usr/bin/env python3.7
+
+
+def cmdValidator(arg_list):
+    """
+    Validates if list arg_list from module tgBot1 contains a valid query string
+    Invalid strings reset the variables valid_product_list and valid_product to zero length and return a zero-length list and string, respectively.
+
+    Valid strings:
+        Only alpha-numeric characters example: eggs bacon
+
+    Invalid Strings:
+        eg1s b@con m*lk
+    """
+
+    error_status = 'valid'
+    valid_product_list = []
+    valid_product = ''
+
+    if (len(arg_list) < 1):
+        #print(f'User Supplied no arguments after /getprice command')
+        error_status = 'empty'
+        valid_product_list = []
+        valid_product = ''
+        #Call sender function here prompt user for non-empty query
+    else:
+        for item in arg_list:
+            if not item.isalpha():
+                #print(f'User supplied item {item} contains non-alphanumeric characters')
+                error_status = 'nonalpha'
+                valid_product_list = []
+                valid_product = ''
+                #Call sender function here prompt user for alphanumeric query
+                break
+
+            valid_product_list.append(item)
+            valid_product = ' '.join(item for item in valid_product_list)
+
+    print(len(valid_product_list))
+    print(len(valid_product))
+    print(error_status)
+    #print(valid_status)  -- removing valid_status, use len[valid_product_list] < 1
+    return valid_product
+
+
+if __name__ == '__main__':
+    cmdValidator(text)
