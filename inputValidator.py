3ff82b0f7e986c0359c098652e1e987e320570ec
Alain Igban <apigban@gmail.com>
2018-09-11 08:02:21 -0400

    1

--- inputValidator.py
+++ inputValidator.py
@@ -1,21 +1,26 @@
 #!/usr/bin/env python3.7
 
+import time
+import pageQueue
 
+#@pageQueue.redisWrite
 def cmdValidator(arg_list):
     """
     Validates if list arg_list from module tgBot1 contains a valid query string
     Invalid strings reset the variables valid_product_list and valid_product to zero length and return a zero-length list and string, respectively.
 
+    Attaches timestamp in milliseconds to request object upon entry to function.
     Valid strings:
         Only alpha-numeric characters example: eggs bacon
 
     Invalid Strings:
         eg1s b@con m*lk
     """
-
+    time_stamp = int(time.time()*1000.0)
     error_status = 'valid'
     valid_product_list = []
     valid_product = ''
+
 
     if (len(arg_list) < 1):
         #print(f'User Supplied no arguments after /getprice command')
@@ -36,10 +41,7 @@ def cmdValidator(arg_list):
             valid_product_list.append(item)
             valid_product = ' '.join(item for item in valid_product_list)
 
-    #print(len(valid_product_list))
-    #print(len(valid_product))
-    #print(error_status)
-    return valid_product, error_status
+    return valid_product, error_status, time_stamp
 
 
 if __name__ == '__main__':
